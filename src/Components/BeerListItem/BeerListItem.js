54cb76f809be1f1d616c9ba1fe908d3db299c0a0
Evgeny Samoylenko <Evgeny.Samoilenko@veeam.com>
2019-04-15 10:42:46 +0300

    little code cleanup

--- src/Components/BeerListItem/BeerListItem.js
+++ src/Components/BeerListItem/BeerListItem.js
@@ -5,14 +5,15 @@ import "./BeerListItem.css";
 class BeerListItem extends Component {
 
 	render() {
+		const {item} = this.props;
 		return (
-			<div key={this.props.item.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
+			<div key={item.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
 				<div className="card">
-					<div className="crop"><img src={this.props.item.image_url} alt={this.props.item.name}/></div>
+					<div className="crop"><img src={item.image_url} alt={item.name}/></div>
 					<div className="card-body">
-						<h5 className="card-title">{this.props.item.name}</h5>
-						<p className="card-text">{this.props.item.description}</p>
-						<Link to={'/beer/' + this.props.item.id}>Go to info</Link>
+						<h5 className="card-title">{item.name}</h5>
+						<p className="card-text">{item.description}</p>
+						<Link to={'/beer/' + item.id}>Go to info</Link>
 					</div>
 				</div>
 			</div>
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -9,10 +9,10 @@ class Footer extends Component {
 				<div className="container">
 					<div className="row">
 						<div className="col text-center">
-							&#9400; <a rel="nofollow" target="_blank" href="https://github.com/SamoilenkoEvgeniy">Samoilenko Evgeny</a>
+							&#9400; <a rel="nofollow noopener noreferrer" target="_blank" href="https://github.com/SamoilenkoEvgeniy">Samoilenko Evgeny</a>
 						</div>
 						<div className="col text-center">
-							With helps <a target="_blank" rel="nofollow" href="https://punkapi.com/">Punk Api</a>
+							With helps <a target="_blank" rel="nofollow noopener noreferrer" href="https://punkapi.com/">Punk Api</a>
 						</div>
 					</div>
 				</div>
